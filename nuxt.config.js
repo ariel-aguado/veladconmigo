59efb41a109098dbfefccd18f136827b12683500
Ariel Aguado <ariel.841207@gmail.com>
2020-10-14 11:24:20 -0400

    Test with opensignal

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,30 +47,11 @@ export default async () => {
       link: [
         { rel: 'preconnect', href: 'https://strapi-velad-conmigo.herokuapp.com' },
         { rel: 'preconnect', href: 'https://res.cloudinary.com' },
-        // { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        // { rel: 'icon', type: 'image/png', sizes: '32x32' },
-        // { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        // { rel: 'manifest', href: '/site.webmanifest' },
-        // { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
-        // {
-        //   rel: 'apple-touch-icon',
-        //   sizes: '180x180',
-        //   href: '/apple-touch-icon.png',
-        //   rel: 'icon',
-        //   type: 'image/png',
-        //   sizes: '32x32',
-        //   href: '/favicon-32x32.png',
-        //   rel: 'icon',
-        //   type: 'image/png',
-        //   sizes: '16x16',
-        //   href: '/favicon-16x16.png',
-        //   rel: 'manifest',
-        //   href: '/site.webmanifest',
-        //   rel: 'mask-icon',
-        //   href: '/safari-pinned-tab.svg',
-        //   color: '#5bbad5',
-        // },
       ],
+      script: [
+        { src: 'https://cdn.onesignal.com/sdks/OneSignalSDK.js', async:"" },
+        { src: './utils/openSignal.js' }
+      ]
     },
     /*
     ** Customize the progress-bar color
